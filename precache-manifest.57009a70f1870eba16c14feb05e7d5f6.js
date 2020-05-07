f8f297fe9a6f4f3c80c3339013b9f10d8c9cb234
AmandaJisoo <amandapark8988@gmail.com>
2020-05-07 07:05:32 -0700

    Updates

---
+++ precache-manifest.57009a70f1870eba16c14feb05e7d5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d88dfa072205af20d87ffb588b9040",
+    "url": "/movie2019/index.html"
+  },
+  {
+    "revision": "7c9b9506a02f2ec1c14d",
+    "url": "/movie2019/static/css/main.9edb2ac0.chunk.css"
+  },
+  {
+    "revision": "d34200c024f3f2e66d2e",
+    "url": "/movie2019/static/js/2.c438bd55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie2019/static/js/2.c438bd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9b9506a02f2ec1c14d",
+    "url": "/movie2019/static/js/main.1a2288e9.chunk.js"
+  },
+  {
+    "revision": "eaf58d6de2b22192641e",
+    "url": "/movie2019/static/js/runtime-main.3f4ad688.js"
+  }
+]);
